a9fa7e8030cf9e0f7aa724c00b680394af5da148
JeongDay <cidd1332@gmail.com>
2025-08-13 11:53:11 +0900

    2025.08.13 수요일 - 오전시간 1
    농작물 심기
    농작물 성장
    농작물 수확
    농작물 획득

---
+++ Assets/05. Farm/02. Scripts/3. Main/Plant/Crop.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class Crop : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
